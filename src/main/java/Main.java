d430ae56d683d09bc290b70742f85e9eb9f369fe
Christopher Wagner <christopher.wagner@secured.team>
2024-12-01 10:19:06 -0500

    add REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        System.out.printf("%s: command not found\n", input);
+        do {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            System.out.printf("%s: command not found\n", input);
+        } while (true);
     }
+
+    // private static String readInput(scanner: Scanner) {
+    //     System.out.print("$ ");
+    //     String input = scanner.nextLine();
+
+    //     return input;
+    // }
+
+    // static void evaluateInput(input: String) {
+        
+    // }
+
+    // void printOutput(command: String) {
+    //     System.out.printf("%s: command not found\n", command);
+    // }
 }
